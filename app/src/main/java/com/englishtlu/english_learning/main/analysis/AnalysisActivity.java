019fa2895597b877f8c7ffd6558b677f1279578c
Đức Minh <ducminhxp.tp@gmail.com>
2024-06-27 00:20:33 +0700

    Update

--- app/src/main/java/com/englishtlu/english_learning/main/analysis/AnalysisActivity.java
+++ app/src/main/java/com/englishtlu/english_learning/main/analysis/AnalysisActivity.java
@@ -81,7 +81,9 @@ public class AnalysisActivity extends AppCompatActivity {
 
         quizRepository = new QuizRepository(this);
 
+        progresses = new ArrayList<>();
 
+        entries = new ArrayList<>();
 
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("QuizResult").child(userId);
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
