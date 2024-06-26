7ddfb80f9c3f477287e4c54ddd85578289cf677b
Đức Minh <ducminhxp.tp@gmail.com>
2024-06-27 00:19:33 +0700

    Update

--- app/src/main/java/com/englishtlu/english_learning/main/MainActivity.java
+++ app/src/main/java/com/englishtlu/english_learning/main/MainActivity.java
@@ -32,7 +32,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private  String lastName;
+    private  String lastName,getLastName;
     private FirebaseAuth auth;
     private int selectedTab = 1;
 
